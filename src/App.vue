b1be7ca0c3a853f74a928a7e275051b2dc93d457
SG-WebDev <sgrobelny.contact@gmail.com>
2020-03-29 16:25:39 +0200

    Create UI, content for home page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <div id="nav" class="nav">
+      <div class="nav__Item">
+        <router-link class="nav__Link" to="/">Home</router-link>
+      </div>
+      <div class="nav__Item">
+        <router-link class="nav__Link" to="/rows-transition">Rows Transition</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+$mainColor: #336e7b;
+$altColor: #558f9d;
+$lightGray: #bdc3c7;
+$textColor: #333333;
+$textAltColor: #ffffff;
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Raleway, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $textColor;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.nav {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  &__Item {
+    background-color: $lightGray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+  &__Link {
+    color: $textAltColor;
+    text-decoration: none;
+    font-size: 1.75rem;
+    flex-basis: 100%;
+    padding: 15px 0;
+  }
+  .router-link-exact-active {
+    background-color: $mainColor;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__Desc">
+      <h1 class="home__Heading">
+        Cipher Studies
+      </h1>
+      <p class="home__Paragraph">
+        This is studies project prepared for <i>Basics of Data Security</i> lessons created by Szymon Grobelny.
+      </p>
+      <p class="home__Paragraph">
+        If you want test implementation of ciphers, algorithms etc. check navigation below. &#8595;
+      </p>
+    </div>
   </div>
 </template>
-
+<style lang="scss">
+  .home {
+    &__Heading {
+      font-size: 3rem;
+      font-weight: 600;
+    }
+    &__Desc {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.25rem;
+    }
+  }
+</style>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
